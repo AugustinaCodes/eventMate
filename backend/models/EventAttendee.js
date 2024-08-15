090b50c87f95047f5f92012916e5b5ce443a6aab
August <76185140+Augustina1995@users.noreply.github.com>
2024-08-15 20:55:06 +0300

    created event attendee model

---
+++ backend/models/EventAttendee.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const eventAttendeeSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  event: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Event",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("EventAttendee", eventAttendeeSchema);
