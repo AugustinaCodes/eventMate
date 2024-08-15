eaf2946dd79120f743c125bfa93bc1c54a8ed74e
August <76185140+Augustina1995@users.noreply.github.com>
2024-08-15 20:57:40 +0300

    index router file and added router to server.js file

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import router from "./routes/index.js"
 
 dotenv.config();
 
@@ -16,5 +17,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(router);
 
 app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
