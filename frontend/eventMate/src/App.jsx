3f33336ae29ad52b0045dcd77a22be4239cb2d64
August <76185140+Augustina1995@users.noreply.github.com>
2024-08-15 21:51:18 +0300

    set up react-router-dom, global scss and variable scss files, started implementing front end structure

--- frontend/eventMate/src/App.jsx
+++ frontend/eventMate/src/App.jsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import Home from "./pages/Home/Home";
+import "./styles/global.scss";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
